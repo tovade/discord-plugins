11dc64d5fc925a3aad3127ded34af6e2b200cffd
tovade <66245404+tovade@users.noreply.github.com>
2021-05-15 20:47:52 +0200

    :hammer: no

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const { Client } = require("../src/index");
 const client = new Client(
   { partials: [] },
   {
-    token: "NzIxMjUyNTgyNTg0MDkwNjU1.XuR0zw.X4CMlyca4ieNR--84ukYI6qcXwQ",
+    token: "",
     prefix: "!",
   }
 );
